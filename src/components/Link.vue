d083af025841cd299e60c27dd574a559deee7882
Septias <scoreplayer2000@gmail.com>
2022-05-18 11:10:22 +0200

    fixed some stuff mentioned by @rosi

---
+++ src/components/Link.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+defineProps({
+  href: String,
+  text: String,
+  icon: String,
+})
+</script>
+
+<template lang="pug">
+a.flex.px-2.items-center.py-1.justify-baseline.no-under-line.text-black(class="hover:bg-gray-200 rounded-full" :href="href")
+  span.mr-1.leading-none {{text}}
+  span(:i="icon")
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -22,30 +22,44 @@ function scrollSecond(a, b) {
 
 <template lang="pug">
 main
-  h1.pl-5.absolute webxdc
+  div.p-5.absolute.flex.w-full.justify-center.border-box.items-center.flex-wrap(class="md:justify-between")
+    h1.m-0.mr-2 webxdc
+    div.flex
+      Link(href="https://github.com/webxdc" icon="carbon-logo-github" text="webxcd")
+      Link(href="https://github.com/webxdc" icon="carbon-book" text="book")
+
+      //a(href="" i="") book
   div.h-screen.flex.items-center.justify-center
     div.downbutton(i="carbon-arrow-down" @click="scrollFirst")
     div#banner.p-2.text-center.rainbow-box.rounded(class="md:p-10 md:m-2")
       h2 Bring messaging to a new level
       p.mb-0 webxdc is empowering developers to create rich interactive content using already known web-technologies and to share them with the world like never before
 
-  div#explanation.flex.p-2.flex-wrap.relative(ref="down1")
+  #explanation.flex.p-2.flex-wrap.relative(ref="down1")
     div.downbutton(i="carbon-arrow-down" @click="scrollSecond")
-    div.flex.flex-col.steps-width.justify-center
+    #steps.flex.flex-col.justify-center
       div
         h2.bold.mb-0.text-3xl.ml-2 Three simple steps:
         div.flex.flex-col.flex-grow.py-5
-          div.extra
-            h3.text-2xl.mt-0 1. Use Version 1.30
-            p The newest deltachat version is available for all mayor platforms.
-          div.extra
+          div.modal
+            h3.text-2xl.mt-0 1. Use DeltaChat version 1.30
+            p The newest deltachat version is available for all major platforms
+              |
+              |
+              a(href="https://delta.chat/en/download") here.
+          div.modal
             h3.text-2xl 2. Develop your own rich content
-            p We created a template for you to start developing cool & exiting rich content now
-            p There are also plenty of examples to get you started
+            p We created a template for you to start developing cool & exciting rich content now.
+            p There are also plenty of
+              |
+              |
+              a(href="https://github.com/webxdc") examples
+              |
+              | to get you started.
               a(href="")
-          div.extra
-            h3.text-2xl 3. Share your App
-            p Just drag and drop your bundled *.xdc file into a chat and share it!
+          div.modal
+            h3.text-2xl 3. Share what you built
+            p Just drag and drop your bundled *.xdc file into a chat and share it with the world!
 
     div.flex.items-center.justify-center.flex-grow(class="md:p-15")
       iframe#video.rounded.shadow-xl(src="https://www.youtube.com/embed/I1K4pBvb2pI" frameborder="0" allowfullscreen)
@@ -53,31 +67,29 @@ main
   div.h-screen.flex.justify-center(ref="down2")
     div.p-2
       h2 Ressources
-      p We created documentation and many examples to get you onboard as easy as possible
+      p We created documentation and many examples to get you startd as easily as possible.
       h2 API
-      p You can finde the API documentation on our official
+      p You can find the API documentation in our official
         |
         |
         a(href="https://github.com/deltachat/deltachat-core-rust/blob/master/draft/webxdc-dev-reference.md")
-          | Github Repo
+          | Github Repo.
       p
-        | We have also written a
+        | For a more thorough introduction see our
         |
-        span
-          a(href="https://deltachat.github.io/webxdc_docs/") book
-        |
-        | for a more thorough introduction
+        a(href="https://deltachat.github.io/webxdc_docs/") book
+
       h2 Examples
       p There are plenty of examples under the webxdc
         |
         |
-        a(href="https://github.com/webxdc") organisation
+        a(href="https://github.com/webxdc") organisation.
 
       h2 Getting Started
-      p To start right off you can clone our
+      p Start right off by cloning our
         |
         |
-        a(href="https://github.com/webxdc/webxdc-dev") boostrap repo
+        a(href="https://github.com/webxdc/webxdc-dev") boostrap repo.
 </template>
 
 <style lang="sass">
@@ -89,29 +101,29 @@ main
 #banner
   width: 40%
 
-.downbutton
-  transition: all 0.3s ease
-  @apply absolute text-3xl bottom-0 right-0 mr-4 mb-5 hover:text-4xl cursor-pointer
-
 @media screen and (max-width: 400px)
   #banner
     width: 100%
     @apply m-2
 
-.steps-width
+.downbutton
+  transition: all 0.3s ease
+  @apply absolute text-3xl bottom-0 right-0 mr-4 mb-5 hover:text-4xl cursor-pointer
+
+#steps
   width: 40%
   p
     @apply ml-4
 
 @media screen and (max-width: 768px)
-  .steps-width
+  #steps
     width: 100%
 
 #video
   width: 100%
   aspect-ratio: 16 / 10
 
-.extra
+.modal
   @apply shadow-xl border-rounded p-5 m-2 bg-white
 
 #explanation
@@ -121,11 +133,11 @@ main
     color: var(--accent)
     margin-top: 0px
 
-  h2
-    //color: #0d3a23
-
 .rainbow-box
   border: 5px solid transparent
   border-image: linear-gradient(to bottom right, #b827fc 0%, #2c90fc 25%, #b8fd33 50%, #fec837 75%, #fd1892 100%)
   border-image-slice: 1
+
+.border-box
+  box-sizing: border-box
 </style>
